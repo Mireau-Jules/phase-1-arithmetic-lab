d220b8c9cba4fd208804653aed4c579a3470d206
Mireau-Jules <saintlouismireaujules@gmail.com>
2025-05-14 12:34:38 -0400

    Complete Lab

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+let num1=2;
+let num2=31;
+const multiply = num1*num2;
+const random = Math.round(Math.random()) + 1;
+const num3= 18;
+const num4= 7;
+const mod = num3 % num4;
+const max =  Math.max(num1, num3,20, num4);
 
-// Write your code here
